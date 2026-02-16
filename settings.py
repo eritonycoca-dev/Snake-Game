4bfe4b032b8e6b2915bb6a4d92aa78319204f26e
eritonycoca-dev <eritonycoca@gmail.com>
2026-02-15 22:44:54 -0500

    Update settings.py
    códigos de las configuraciones, colores y tamaño

---
+++ settings.py
@@ -0,0 +1,17 @@
+# Dimensiones de la ventana 
+WIDTH = 800 
+HEIGHT = 600 
+
+#Tamaño de cada bloque de la serpiente 
+BLOCK_SIZE = 20
+
+# FPS (velocidad del juego)
+FPS = 10 
+
+# Colores 
+WHITE = (255, 255, 255)
+GREEN = (34, 139, 34)
+DARK_GREEN = (0, 100, 0)
+RED = (200, 0, 0)
+BLACK = (0, 0, 0)
+YELLOW = (255, 255, 0)
