58a5c17aa1a51c2fc6e54f5bc9a0e3b73d202ddd
eritonycoca-dev <eritonycoca@gmail.com>
2026-02-20 16:10:47 -0500

    Update snake.py
    Cambios realizados:
    1. Sistema de dificultad dinámica.
    2. Control inteligente de dirección. 
    3. Serpiente con degradado dinámico.

--- snake.py
+++ snake.py
@@ -1,5 +1,5 @@
-import pygame 
-from settings import BLOCK_SIZE, GREEN 
+import pygame  
+from settings import BLOCK_SIZE, Initial_speed , Speed_Increment, Level_Up_Food 
 
 class Snake: 
     def __init__(self, name): 
@@ -7,6 +7,8 @@ class Snake:
         self .body = [(100, 100), (80, 100), (60, 100)]
         self .direction = "RIGHT"
         self .grow = False 
+        self.food_eaten= 0
+        self.speed = Initial_speed 
     
     def move(self):
         head_x, head_y = self.body[0]
@@ -25,7 +27,14 @@ class Snake:
         if not self.grow:
             self.body.pop()
         else: 
-            self.grow = False 
+            self.grow = False
+
+    def grow_snake(self):
+        self.grow=True 
+        self.food_eaten += 1
+
+        if self.food_eaten % Level_Up_Food  == 0:
+            self.speed += Speed_Increment  
 
     def change_direction(self, new_direction): 
         opposite = {
@@ -37,13 +46,17 @@ class Snake:
         if new_direction != opposite[self.direction]: 
             self.direction = new_direction
 
-    def draw(self, surface):
-        for segment in self.body: 
-            pygame.draw.rect(surface, GREEN,
-                             (segment[0], segment[1], BLOCK_SIZE, BLOCK_SIZE))
-            
     def check_self_collision(self):
         return self.body[0] in self.body[1:]
+    
+    def draw(self, surface):
+        # Degradado dinámico
+        for index, segment in enumerate(self.body):
+            green_intesity = max(50,255 - index * 10)
+            color=(0, green_intesity, 0)
+            pygame.draw.rect(surface, color,
+                             (segment[0], segment[1], BLOCK_SIZE, BLOCK_SIZE)) 
+    
     
     def grow_snake(self):
         self.grow = True 
