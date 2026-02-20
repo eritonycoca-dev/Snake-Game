f9fbc717764a71c407e4e088fc44ece8ee47bbc7
eritonycoca-dev <eritonycoca@gmail.com>
2026-02-20 16:08:47 -0500

    Update food.py
    Cambios y mejora:
    1.  Validación avanzada de la serpiente, ya no hay la posibilidad de que se genere comida encima de la serpiente. 
    2. Junto con la implementación de obstáculos se garantiza que la serpiente no aparezca encima de obstáculos. 
    3. Mejor algoritmo espacial.

--- food.py
+++ food.py
@@ -6,17 +6,20 @@ import pygame
 
 class Food: 
 
-    def __init__(self, snake_body):
-        self.position = self.generate_position(snake_body)
+    def __init__(self, snake_body, obstacles):
+        self.position = self.generate_position(snake_body, obstacles)
 
-    def generate_position(self, snake_body):
-        while True: 
+    def generate_position(self, snake_body, obstacles):
+          while True: 
             x = random.randarage(0, WIDTH, BLOCK_SIZE)
             y = random.randrange(0, HEIGHT, BLOCK_SIZE)
 
-            if (x, y) not in snake_body: 
+            if (x, y) not in snake_body and (x, y) not in obstacles: 
                 return (x, y)
     
     def draw(self, surface): 
         pygame.draw.rect(surface, RED,
-                         (self.position[0], self.position[1], BLOCK_SIZE, BLOCK_SIZE))
+                         (self.position[0], self.position[1], 
+                          BLOCK_SIZE, BLOCK_SIZE))
+        
+
