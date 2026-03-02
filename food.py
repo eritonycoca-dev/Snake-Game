54273fa0a6a27ff05508a2a07ce68afcc4222ba1
eritonycoca-dev <eritonycoca@gmail.com>
2026-03-01 22:50:38 -0500

    Update food.py

--- food.py
+++ food.py
@@ -1,25 +1,25 @@
-# food.py 
+import random
+import pygame
+from settings import WIDTH, HEIGHT, BLOCK_SIZE, RED
 
-import random 
-from settings import WIDTH, HEIGHT, BLOCK_SIZE, RED 
-import pygame 
 
-class Food: 
+class Food:
+    def __init__(self, snake_body, obstacles_positions):
+        self.position = self.generate_position(snake_body, obstacles_positions)
 
-    def __init__(self, snake_body, obstacles):
-        self.position = self.generate_position(snake_body, obstacles)
+    def generate_position(self, snake_body, obstacles_positions):
+        while True:
+            x = random.randrange(0, WIDTH // BLOCK_SIZE) * BLOCK_SIZE
+            y = random.randrange(0, HEIGHT // BLOCK_SIZE) * BLOCK_SIZE
+            pos = (x, y)
+            
+            if pos not in snake_body and pos not in obstacles_positions:
+                return pos
 
-    def generate_position(self, snake_body, obstacles):
-          while True: 
-            x = random.randarage(0, WIDTH, BLOCK_SIZE)
-            y = random.randrange(0, HEIGHT, BLOCK_SIZE)
-
-            if (x, y) not in snake_body and (x, y) not in obstacles: 
-                return (x, y)
-    
-    def draw(self, surface): 
+    def draw(self, surface):
         pygame.draw.rect(surface, RED,
-                         (self.position[0], self.position[1], 
+                         (self.position[0], self.position[1],
                           BLOCK_SIZE, BLOCK_SIZE))
         
+
 
